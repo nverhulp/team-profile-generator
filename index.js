0cb15feb235c1ff77ec4df7b98594563b54fad4d
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-15 23:41:41 -0700

    profile variables created

---
+++ index.js
@@ -0,0 +1,7 @@
+const generateHTML = require('./utils/generateHTML');
+
+// profile variables
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+
