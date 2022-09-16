0b8897e06ebd6b1e1b079c546936d783ad26af14
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-15 23:58:54 -0700

    array of questions for manager

--- index.js
+++ index.js
@@ -1,7 +1,76 @@
+// packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 const generateHTML = require('./utils/generateHTML');
 
 // profile variables
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
+const teamArray = [];
+
+// array of questions for user input
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the manager?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('You must enter the name of the manager');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('You must enter the ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('You must enter an email address');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the manager's office number?",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log('You must enter the office number');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, office } = managerInput;
+        const manager = new Manager (name, id, email, office);
+
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
