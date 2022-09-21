5c79cac9c73127011c08146cc4f1725d6f895e87
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-20 20:47:43 -0700

    functin to create html file

--- index.js
+++ index.js
@@ -103,27 +103,27 @@ function addMember() {
     });
 }
 
-
-
 // function to create HTML page file
-const writeFile = data => {
-    fs.writeFile('./dist/index.html', data, err => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Your team profile has been created');
-        }
-    })
-};
-
-addManager()
-    .then(addEmployee)
-    .then(teamArray => {
-        return generateHTML(teamArray);
-    })
-    .then(pageHTML => {
-        return writeFile(pageHTML);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+function startHTML() {
+    return `
+        const html = <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Team Profile</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-dark bg-dark mb-5">
+                <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+            </nav>
+            <div class="container">
+                <div class="row">`;
+        fs.writeFile("./output/index.html", html, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        console.log("start");
+}
