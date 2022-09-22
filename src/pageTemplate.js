694046c5c658e64c7e1d4507410deff24ed06a07
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-22 12:24:28 -0700

    const generateEngineer and const generateIntern

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -19,11 +19,39 @@ const generateTeam = team => {
     }
 
     const generateEngineer = engineer => {
-
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">Office Number: ${engineer.getGitHub()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
     }
 
     const generateIntern = intern => {
-
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">Office Number: ${intern.getSchoolName()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
     }
 }
 
