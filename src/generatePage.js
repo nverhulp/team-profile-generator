719d83c2f2131363136ef5ec69e733690569a8e9
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-22 17:36:41 -0700

    fixed bugs and added styling

--- src/generatePage.js
+++ src/generatePage.js
@@ -1,57 +1,58 @@
 const generateManager = manager => {
     return`
-    <div class="card employee-card">
-    <div class="card-header">
-            <h2 class="card-title">${manager.name}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
-        </div>
-        <div class="card-body">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4>
+            </div>
+
             <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${manager.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto: ${manager.email}">${manager.email}</a></li>
-                    <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
-                </ul>
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
             </div>
         </div>
-        `;
+    </div>
+    `;
 }
+           
 const generateEngineer = engineer => {
     return`
-    <div class="card employee-card">
-    <div class="card-header">
-            <h2 class="card-title">${engineer.name}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Engineer</h3>
-        </div>
-        <div class="card-body">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4>
+            </div>
+
             <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${engineer.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto: ${engineer.email}">${engineer.email}</a></li>
-                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
-                </ul>
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="office">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
             </div>
         </div>
-        `;
+    </div>
+    `;
 }
 
 const generateIntern = intern => {
     return`
-    <div class="card employee-card">
-    <div class="card-header">
-            <h2 class="card-title">${intern.name}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Intern</h3>
-        </div>
-        <div class="card-body">
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4>
+            </div>
+
             <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${intern.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto: ${intern.email}">${intern.email}</a></li>
-                    <li class="list-group-item">School Name: ${intern.school}</li>
-                </ul>
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="office">School Name: ${intern.school}</p>
             </div>
         </div>
-        `;
+    </div>
+    `;
 }
 const generatePage = employeeArr => {
     empList = [];
@@ -76,40 +77,43 @@ const generatePage = employeeArr => {
 }
 
 const HTMLPage = mergedTeam =>{
-    return `
+    return`
     <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <header>
+          <nav class="navbar" id="navbar">
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <div class="container">
+              <div class="row justify-content-center" id="team-cards">
+                  <!--Team Cards-->
+                  ${mergedTeam}
+              </div>
+          </div>
+      </main>
+      
+    </body>
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${mergedTeam}
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
     `;
 }
+
 
 
 // export to index.html
